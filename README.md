bc18b226ab8801ff571a5853e506d514f331d05e
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-04-13 04:48:52 +0000

    get server working, create readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hackatime-badge
 An API that converts a Hack Club Slack ID and Hackatime project name to a format for Shields.io badges!
+
+# How to run locally
+1. Install needed dependencies (`npm i`)
+2. Edit PORT variable in server.js as needed and run server.js
+
+# Usage
+URL to fetch: `https://img.shields.io/endpoint?url=https://badge.pbhak.hackclub.app/{your Slack user ID}/{project name}`
+
+If adding to a Markdown file, just use standard image syntax: `![Endpoint Badge](https://img.shields.io/endpoint?url=https://badge.pbhak.hackclub.app/{your Slack user ID}/{project name})`
--- server.js
+++ server.js
@@ -3,11 +3,18 @@ import express from 'express';
 const PORT = 41597;
 const server = express();
 
-server.get('/badge/:id/:project', async (req, res) => {
-  const stats_url = `https://hackatime.hackclub.com/api/v1/users/${req.params.id}/stats?features=projects`;
-  const project_info = (await fetch(stats_url)).json().projects;
+function badge_data(time) {
+  return JSON.stringify({ schemaVersion: 1, label: 'hackatime', message: time })
+}
 
-  console.log(project_info);
+server.get('/:id/:project', async (req, res) => {
+  const stats_url = `https://hackatime.hackclub.com/api/v1/users/${req.params.id}/stats?features=projects`;
+  const user_info = await fetch(stats_url);
+  const project_time = (await user_info.json()).data.projects
+    .filter((project_info) => project_info.name == req.params.project)[0].text
+
+  res.send(badge_data(project_time));
 });
 
 server.listen(PORT)
+console.log(`started on port ${PORT}`)
