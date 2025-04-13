322766a428c913f51528283665205d57af49cfe9
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-04-12 22:04:31 -0700

    add badge request

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ import express from 'express';
 const PORT = 41597;
 const server = express();
 
-function badge_data(time) {
-  return JSON.stringify({ schemaVersion: 1, label: 'hackatime', message: time })
-}
-
 server.get('/:id/:project', async (req, res) => {
   const stats_url = `https://hackatime.hackclub.com/api/v1/users/${req.params.id}/stats?features=projects`;
   const project_time = await fetch(stats_url).then(async response => {
     return (await response.json()).data.projects
       .filter((project_info) => project_info.name == req.params.project)[0].text
-  })
+  });
 
-  res.send(badge_data(project_time));
+  await fetch(`https://img.shields.io/badge/hackatime-${project_time}-${req.params.color ?? 'blue'}`).then(response => {
+    res.send(response);
+  });
 });
 
-server.listen(PORT)
-console.log(`started on port ${PORT}`)
+server.listen(PORT);
+console.log(`started on port ${PORT}`);
