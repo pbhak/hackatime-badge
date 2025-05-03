56022e0d2aa6c098c7aa894d21c1da292d2626ee
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-05-03 12:44:21 -0700

    fix: add aliases to projects (#1)

--- server.js
+++ server.js
@@ -3,51 +3,54 @@ import express from "express";
 const PORT = process.env.port || 3000;
 const server = express();
 
-let requests = {
-  totalRequests: 0,
-  requestsInLast24Hours: 0,
-  requestsInLast7Days: 0,
-  requestTimeStamps: [],
-};
+function secondsToHours(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
 
-server.use("/:id/:project", (req, res, next) => {
-  requests.requestTimeStamps.push(Date.now());
-  requests.totalRequests++;
+  return (hours ? hours + "h " : "") + minutes + "m";
+}
 
-  const oneDayAgo = Date.now() - 24 * 60 * 60 * 1000;
-  const oneWeekAgo = Date.now() - 7 * 24 * 60 * 60 * 1000;
+async function getSecondsForProject(userId, projectName) {
+  const stats_url = `https://hackatime.hackclub.com/api/summary?user=${userId}`;
 
-  requests.requestsInLast24Hours = requests.requestTimeStamps.filter((ts) => ts > oneDayAgo).length;
-  requests.requestsInLast7Days = requests.requestTimeStamps.filter((ts) => ts > oneWeekAgo).length;
+  const project = await fetch(stats_url)
+    .then(async (response) => await response.json())
+    .then((json) => json.projects)
+    .then((projects) =>
+      projects.find((project) => project.key === projectName)
+    );
 
-  next();
-});
+  return project ? project.total : 0;
+}
 
 server.get("/", (req, res) =>
   res.redirect("https://github.com/pbhak/hackatime-badge")
 );
 
 server.get("/:id/:project", async (req, res) => {
-  const stats_url = `https://hackatime.hackclub.com/api/v1/users/${req.params.id}/stats?features=projects`;
   const label = req.query.label ?? "hackatime";
   const color = req.query.color ?? "blue";
+  const aliases = req.query.aliases ? req.query.aliases.split(",") : [];
 
-  const project_index = await fetch(stats_url).then(async (response) => {
-    try {
-      const projects = (await response.json()).data.projects;
-      return projects.indexOf(
-        projects.find((project) => project.name == req.params.project)
-      );
-    } catch {
-      res.status(400);
-      res.send("Error processing request - is your information correct? (400)");
-    }
-  });
+  let projectTime = await getSecondsForProject(
+    req.params.id,
+    req.params.project
+  );
+
+  if (projectTime === 0) res.sendStatus(400);
+
+  for (const alias of aliases) {
+    await getSecondsForProject(req.params.id, alias).then(
+      (seconds) => (projectTime += seconds)
+    );
+  }
 
   res.set("Content-Type", "image/svg+xml");
   res.set("Cache-Control", "no-cache");
 
-  const shields_url = `https://img.shields.io/badge/dynamic/json?url=${stats_url}&query=$.data.projects[${project_index}].text&label=${label}&color=${color}`;
+  const shields_url = `https://img.shields.io/badge/${label}-${secondsToHours(
+    projectTime
+  )}-${color}`;
 
   try {
     await fetch(shields_url).then(
@@ -58,10 +61,6 @@ server.get("/:id/:project", async (req, res) => {
     res.status(500);
     res.send("Error fetching info from shields.io server (500)");
   }
-});
-
-server.get("/analytics", (req, res) => {
-  res.send(requests);
 });
 
 server.listen(PORT);
